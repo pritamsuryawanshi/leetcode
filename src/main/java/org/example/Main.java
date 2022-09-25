022d1910021c1e2b270bbdeab2c4dca809a86ba5
pritamsuryawanshi <sailiblo@1997>
2022-09-25 22:57:42 +0530

    Added-> A code to sort people by their heights, both provided in seperate arrays

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] names = {"Mary", "John", "Emma"};
+        int[] heights = {180, 165, 170};
+        String[] sortedPeople = new Main().sortPeople(names, heights);
+        System.out.println(Arrays.toString(sortedPeople));
+    }
+
+    //sort people by their heights
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer, String> myMap = new HashMap<>();
+        for (int i = 0; i < heights.length; i++) {
+            myMap.put(heights[i], names[i]);
+        }
+        Arrays.sort(heights);
+        System.out.println(Arrays.toString(heights));
+        for (int i = 0; i < heights.length; i++) {
+            names[i] = myMap.get(heights[heights.length - 1 - i]);
+        }
+        return names;
     }
 }
